4699be8a5d5eb101288d50ab94cd4069d815059f
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-20 23:46:43 +0800

    15 List of Different Color

---
+++ color_list.py
@@ -0,0 +1,9 @@
+Color_list = ["Black", "Grey", "Red", "Blue", "Orange", "White", "Brown", "Pink", "Yellow", "Green", "Navy blue", "Maroon", "Sİlver", "Dark blue", "Aqua"]
+print("A. Entire list:", Color_list)
+print("B. 10th index is:", Color_list[9])
+Color_list[3] = "Purple"
+print("C. Updaterd List:", Color_list)
+del Color_list[4]
+print("D. Updated List:", Color_list)
+print("E. Sliced Portion:", Color_list[2:8])
+print("F. Last Index Is:", Color_list[-1])
