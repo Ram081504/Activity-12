c2a6c155000b7e57c6671f65c5f77bfd0a8ff1c7
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-20 23:09:32 +0800

    10 Different Names of Animals

---
+++ animal_list.py
@@ -0,0 +1,8 @@
+Animal_list = ["Cat", "Dog", "Lion", "Bear", "Cow", "Horse", "Camel", "Chicken", "Goat", "Leopard"]
+print("A. Entire list:", Animal_list)
+print("B. 3rd index is:", Animal_list[2])
+Animal_list[5] = "Elephant"
+print("C. Updaterd List:", Animal_list)
+del Animal_list[7]
+print("D. Updated List:", Animal_list)
+print("E. Last Index Is:", Animal_list[-1])
