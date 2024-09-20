6284ee43d2649f4999aed78396eca30da64cd6d0
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-21 00:22:27 +0800

    List of Vegetables

---
+++ vegetable_list.py
@@ -0,0 +1,8 @@
+Vegetable_list = ["Broad Beans", "Broccoli", "Carrots", "Garlic", "Green Beans", "Lettuce", "Mushrooms"]
+print("A. Entire list:", Vegetable_list)
+print("B. 5th index is:", Vegetable_list[4])
+Vegetable_list[2] = "Spinach"
+print("C. Updaterd List:", Vegetable_list)
+del Vegetable_list[5]
+print("D. Updated List:", Vegetable_list)
+print("F. Last Index Is:", Vegetable_list[-1])
