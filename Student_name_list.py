fd60cad1efaf5867e0c9bec323f874c959a4721d
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-20 22:46:27 +0800

    20 List of Student Names

---
+++ Student_name_list.py
@@ -0,0 +1,9 @@
+Student_names = ["SEVILLA", "TAAY", "TAYAS", "TORIBIO", "AMORES", "RODIEL", "INAO", "AVELINO", "BECHAYDA", "DIONGZON", "ENDONELA", "DULOS", "HERRERA", "LAYLO", "PASAOA", "BITANGCOR", "CALCETAS", "FRANCISCO", "MELLIZA", "CARPIO"]
+print("A. Entire list:", Student_names)
+print("B. 15th index is:", Student_names[14])
+Student_names[11] = "DIO"
+print("C. Updaterd List:", Student_names)
+del Student_names[9]
+print("D. Updated List:", Student_names)
+print("E. Sliced Portion:", Student_names[2:5])
+print("F. Last Index Is:", Student_names[-1])
