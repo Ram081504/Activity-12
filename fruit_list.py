b791152bb096c1e7588620d9aa8169c495f4eddc
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-21 00:08:22 +0800

    The list of 12 Fruits

---
+++ fruit_list.py
@@ -0,0 +1,9 @@
+Fruits_list = ["Apple", "Grape", "Oranges", "Bananas", "Lemon", "Kiwi", "Pineapple", "Strawberries", "Watermelon", "Avocado", "Cherries", "Pear"]
+print("A. Entire list:", Fruits_list)
+print("B. 9th index is:", Fruits_list[8])
+Fruits_list[1] = "Mango"
+print("C. Updated List:", Fruits_list)
+del Fruits_list[9]
+print("D. Updated List:", Fruits_list)
+print("E. Sliced Portion:", Fruits_list[4:7])
+print("F. Last Index Is:", Fruits_list[-1])
