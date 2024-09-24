fc6793a75b85d4be4a945455fc6e6b2304b7ab61
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-24 15:18:39 +0800

    Movie List

---
+++ movie_list.py
@@ -0,0 +1,9 @@
+Movie_list = ["Star Wars", "The Public Enemy", "Army of Darkness", "Hit Man", "Gladiator 2", "Inside Out 2", "The Fall Guy", " Godzilla x Kong", "Despicable Me 4", "Kung Fu panda 4", "Dune: Part Two", "Argylle", "Twistes", "Challengers", "Civil War", "Madame Web", "Deadpool & Wolverine", "IF", "Alien: Romulus", "Kingdom of the Planet of the Apes"]
+print("A. Entire list:", Movie_list)
+print("B. 12th index is:", Movie_list[11])
+Movie_list[14] = "Inception"
+print("C. Updated List:", Movie_list)
+del Movie_list[14]
+print("D. Updated List:", Movie_list)
+print("E. Sliced Portion:", Movie_list[2:5])
+print("F. Last Index Is:", Movie_list[-1])
