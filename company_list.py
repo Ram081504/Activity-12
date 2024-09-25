81819993323c476f336722ab6dc3395d193e369f
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-25 15:24:52 +0800

    Top Largest Companies in the World

---
+++ company_list.py
@@ -0,0 +1,8 @@
+Company_list = ["Walmar Inc.", "Amazon.com Inc.", "China Petroleum & Chemical Corp.", "PetroChina Co. Ltd.", "Berkshire Hathaway", "UnitedHealth Group Inc.", "CVS Health Corp.", "Volkswagen AG", "Exxon Mobil Corp.", "Microsoft"]
+print("A. Entire list:", Company_list)
+print("B. 7th index is:", Company_list[6])
+Company_list[4] = "Apple"
+print("C. Updated List:", Company_list)
+del Company_list[8]
+print("D. Updated List:", Company_list)
+print("F. Last Index Is:", Company_list[-1])
