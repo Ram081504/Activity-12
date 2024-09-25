37b08dbfa84d6758caebd7862e5bb177bedc5719
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-25 15:11:25 +0800

    Tallest Mountain list

---
+++ mountain_list.py
@@ -0,0 +1,8 @@
+Author_list = ["Dhaulagiri", "Makalu", "Manaslu", "Kangchenjunga", "Cho Oyu", "Lhotse", "Nanga Parbat", "K2"]
+print("A. Entire list:", Author_list)
+print("B. 5th index is:", Author_list[4])
+Author_list[2] = "Everest"
+print("C. Updated List:", Author_list)
+del Author_list[5]
+print("D. Updated List:", Author_list)
+print("F. Last Index Is:", Author_list[-1])
