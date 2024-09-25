3f1667b9982e0c360c744dd0c4109b292f7154a9
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-25 11:53:35 +0800

    updated game_list.py

---
+++ game_list.py
@@ -0,0 +1,9 @@
+Game_list = ["GTA", "Uncharted 2", "Call of Duty 4", "Portal", "The Last of Us", "God of War", "Resident Evil 4", "Final Fantasy VI", "GoldenEye 007", "Super Mario World", "Batman: Arkham City", "Hades", "Civilization IV", "INSIDE", "Eldin Ring"]
+print("A. Entire list:", Game_list)
+print("B. 7th index is:", Game_list[6])
+Game_list[3] = "Minecraft"
+print("C. Updated List:", Game_list)
+del Game_list[8]
+print("D. Updated List:", Game_list)
+print("E. Sliced Portion:", Game_list[4:9])
+print("F. Last Index Is:", Game_list[-1])
