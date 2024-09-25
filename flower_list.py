3309effe1e0483137ba13a594180d880763d1efb
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-25 12:38:40 +0800

    List of Beautiful Flowers

---
+++ flower_list.py
@@ -0,0 +1,8 @@
+Flower_list = ["Cherry Blossom", "Orchid", "Lily", "Jasmine", "Sunflower", "Iris", "Tulip", "Dahlia", "Daisy", "Azalea"]
+print("A. Entire list:", Flower_list)
+print("B. 5th index is:", Flower_list[4])
+Flower_list[7] = "Rose"
+print("C. Updated List:", Flower_list)
+del Flower_list[1]
+print("D. Updated List:", Flower_list)
+print("F. Last Index Is:", Flower_list[-1])
