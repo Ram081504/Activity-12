8332157a529d05e2a422d2e373bcac16932fc5fb
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-25 11:49:37 +0800

    Update animal_list.py

--- animal_list.py
+++ animal_list.py
@@ -2,7 +2,7 @@ Animal_list = ["Cat", "Dog", "Lion", "Bear", "Cow", "Horse", "Camel", "Chicken",
 print("A. Entire list:", Animal_list)
 print("B. 3rd index is:", Animal_list[2])
 Animal_list[5] = "Elephant"
-print("C. Updaterd List:", Animal_list)
+print("C. Updated List:", Animal_list)
 del Animal_list[7]
 print("D. Updated List:", Animal_list)
 print("E. Last Index Is:", Animal_list[-1])
