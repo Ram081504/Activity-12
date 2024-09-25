2d09b2f99842f665e3660e9255126cc1f368b604
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-25 12:28:43 +0800

    Programming languages

---
+++ language_list.py
@@ -0,0 +1,9 @@
+Language_list = ["PHP", "C++", "Perl", "Ruby", "C#", "JavaScript", "Java", "Rust", "Go", "Swift", "C", "R", "TypeScript", "Kotlin" ]
+print("A. Entire list:", Language_list)
+print("B. 11th index is:", Language_list[10])
+Language_list[8] = "Python"
+print("C. Updated List:", Language_list)
+del Language_list[12]
+print("D. Updated List:", Language_list)
+print("E. Sliced Portion:", Language_list[4:11])
+print("F. Last Index Is:", Language_list[-1])
