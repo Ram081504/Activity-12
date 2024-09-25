25c40cea74227ce54764be718c631df800133a06
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-25 16:25:49 +0800

    Most Significant Figure

---
+++ historical_figures_list.py
@@ -0,0 +1,8 @@
+historical_figures_list = ["Napoleon Bonaparte", "Abraham Lincoln", "Geoge Washington", "Martin Luther King Jr.", "Mahatma Gandhi", "Thomas Jefferson", "William Shakespeare", "Albert Einstein", "Adolf Hitler", "Aristotle"]
+print("A. Entire list:", historical_figures_list)
+print("B. 8th index is:", historical_figures_list[7])
+historical_figures_list[3] = "Nelson Mandela"
+print("C. Updated List:", historical_figures_list)
+del historical_figures_list[6]
+print("D. Updated List:", historical_figures_list)
+print("F. Last Index Is:", historical_figures_list[-1])
