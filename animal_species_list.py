dd92dc416abf59130411d0f291987926454a320c
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-25 16:14:29 +0800

    Biological Species

---
+++ animal_species_list.py
@@ -0,0 +1,8 @@
+Animal_Species_list = ["Blue Whale", "African bush elephant", "Tiger", "Chimpanzee", "Vaquita", "Bonobo", "Human"]
+print("A. Entire list:", Animal_Species_list)
+print("B. 2nd index is:", Animal_Species_list[1])
+Animal_Species_list[4] = "Gorilla"
+print("C. Updated List:", Animal_Species_list)
+del Animal_Species_list[5]
+print("D. Updated List:", Animal_Species_list)
+print("F. Last Index Is:", Animal_Species_list[-1])
