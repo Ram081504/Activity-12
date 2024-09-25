23e207ac3d69b94c6bdbcee841726a350e7e871a
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-25 13:00:12 +0800

    Top Spoken Languages in the World

---
+++ language_spoken_list.py
@@ -0,0 +1,9 @@
+Language_Spoken_list = ["English", "Mandarin Chinese", "Hindi", "Yue Chinese", "French", "Arabic", "Bengali", "Russian", "Portuguese", "Urdu", "Indonesian", "German", "Japanese", "Nigerian Pidgin", "Marathi", "Telugu", "Turkish", "Tamil", "Cantonese", "Vietnamese"]
+print("A. Entire list:", Language_Spoken_list)
+print("B. 13th index is:", Language_Spoken_list[12])
+Language_Spoken_list[9] = "Spanish"
+print("C. Updated List:", Language_Spoken_list)
+del Language_Spoken_list[15]
+print("D. Updated List:", Language_Spoken_list)
+print("E. Sliced Portion:", Language_Spoken_list[5:10])
+print("F. Last Index Is:", Language_Spoken_list[-1])
