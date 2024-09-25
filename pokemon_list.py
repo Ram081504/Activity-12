780e0f0faf1574bb062e51ee5fad1c0c29f42cc0
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-25 15:43:45 +0800

    Pokemon Collection List

---
+++ pokemon_list.py
@@ -0,0 +1,9 @@
+Pokemon_list = ["Bulbasaur", "Charmander", "Squirtle", "Caterpie", "Weedle", "Pidgey", "Rattata", "Spearow", "Ekans", "Raichu", "SandShrew", "Nidorina", "Clefairy", "Vulpix", "Zubat"]
+print("A. Entire list:", Pokemon_list)
+print("B. 9th index is:", Pokemon_list[8])
+Pokemon_list[11] = "Pikachu"
+print("C. Updated List:", Pokemon_list)
+del Pokemon_list[9]
+print("D. Updated List:", Pokemon_list)
+print("E. Sliced Portion:", Pokemon_list[3:6])
+print("F. Last Index Is:", Pokemon_list[-1])
