afa866ef954037392fa50065153fb4e1c345340d
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-25 12:17:48 +0800

    8 planet list

---
+++ planet_list.py
@@ -0,0 +1,8 @@
+Planet_list = ["Mercury", "Mars", "Neptune", "Venus", "Jupiter", "Earth", "Uranus", "Saturn"]
+print("A. Entire list:", Planet_list)
+print("B. 3th index is:", Planet_list[2])
+Planet_list[6] = "Pluto"
+print("C. Updated List:", Planet_list)
+del Planet_list[3]
+print("D. Updated List:", Planet_list)
+print("F. Last Index Is:", Planet_list[-1])
