e35fc7bde53de63bd0572f84edc9e55e7c1687d9
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-25 12:49:12 +0800

    Greatest Authors of All-Time

---
+++ author_list.py
@@ -0,0 +1,9 @@
+Author_list = ["Ernest Hemingway", "J. K. Rowling", "Oscar Wilder", "William Shakespeare", "Ayn Rand", "Edgar Allan Poe", "George Eliot", "William Faulkner", "James Joyce", "Leo Tolstoy", "Lewis Carroll", "Maya Angelou"]
+print("A. Entire list:", Author_list)
+print("B. 6th index is:", Author_list[5])
+Author_list[3] = "Mark Twain"
+print("C. Updated List:", Author_list)
+del Author_list[9]
+print("D. Updated List:", Author_list)
+print("E. Sliced Portion:", Author_list[2:8])
+print("F. Last Index Is:", Author_list[-1])
