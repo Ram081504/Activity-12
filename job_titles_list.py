a2d9cd430a4cef31cad9d513c9e8e8a54d0fa666
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 18:22:48 +0800

    Job Titles for IT Graduates

---
+++ job_titles_list.py
@@ -0,0 +1,8 @@
+Job_Titles_list = ["Cloud infrastructure architect", "IT systems architect", "Computer systems manager", "Systems analyst", "Network administrator", "Database administrator", "Data analyst", "Back-end developer", "Front-end developer", "Full-stack developer"]
+print("A. Entire list:", Job_Titles_list)
+print("B. 6th index is:", Job_Titles_list[5])
+Job_Titles_list[4] = "Software Engineer"
+print("C. Updated List:", Job_Titles_list)
+del Job_Titles_list[8]
+print("D. Updated List:", Job_Titles_list)
+print("F. Last Index Is:", Job_Titles_list[-1])
