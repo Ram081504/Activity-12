cfb60c5596a6f884dc522b1f28d8f6db8f4b2553
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 18:09:23 +0800

    List of Animal Habitats

---
+++ animal_habitats_list.py
@@ -0,0 +1,8 @@
+Animal_Habitats_list = ["Grassland", "Desert", "Forest", "Wetlands", "Ocean", "Polar Regions", "Temperate Forest", "Tropical Rainforest"]
+print("A. Entire list:", Animal_Habitats_list)
+print("B. 5th index is:", Animal_Habitats_list[4])
+Animal_Habitats_list[2] = "Savannah"
+print("C. Updated List:", Animal_Habitats_list)
+del Animal_Habitats_list[5]
+print("D. Updated List:", Animal_Habitats_list)
+print("F. Last Index Is:", Animal_Habitats_list[-1])
