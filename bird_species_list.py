c4a9a667f12806a58fdc042dc23d76bf79815540
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 19:58:01 +0800

    List of Bird Species

---
+++ bird_species_list.py
@@ -0,0 +1,9 @@
+Bird_Species_list = ["Eurasian Teal", "House Sparrow", "Hoatzin", "Common Waxbill", "Eurasian Penduline Tit", "Barn Swallow", "Red-breasted merganser", "Nothern Pintail", "Peregrine Falcon", "Harpy Eagle", "Song Thrush", "Kagu"]
+print("A. Entire list:", Bird_Species_list)
+print("B. 9th index is:", Bird_Species_list[8])
+Bird_Species_list[5] = "Eagle"
+print("C. Updated List:", Bird_Species_list)
+del Bird_Species_list[7]
+print("D. Updated List:", Bird_Species_list)
+print("E. Sliced Portion:", Bird_Species_list[2:7])
+print("F. Last Index Is:", Bird_Species_list[-1])
