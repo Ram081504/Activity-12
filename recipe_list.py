e598b15b97f0803b17b8d8f6031c095f81d09ddc
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 22:04:46 +0800

    Delicious Food Recipes

---
+++ recipe_list.py
@@ -0,0 +1,9 @@
+Recipe_list = ["Turkey Meatloaf", "Stuffed Peppers", "Chicken-Fried Steak", "Blueberry Muffins", "Cheesy Potatoes", "Baked Salmon", "Chewy Chocolate Chip Cookies", "Pot Roast", "Beer Batter", "Classic Strawberry Shortcake", "Quiche Lorraine", "Caesar Salad", "Scrambled Eggs", "Chicken Cutlets", "Hummus"]
+print("A. Entire list:", Recipe_list)
+print("B. 12th index is:", Recipe_list[11])
+Recipe_list[9] = "Lasagna"
+print("C. Updated List:", Recipe_list)
+del Recipe_list[10]
+print("D. Updated List:", Recipe_list)
+print("E. Sliced Portion:", Recipe_list[4:10])
+print("F. Last Index Is:", Recipe_list[-1])
