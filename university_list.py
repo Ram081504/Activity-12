28e7acd210d8d8f664f51dee3d1533f752f0d0d7
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 22:58:18 +0800

    Top University

---
+++ university_list.py
@@ -0,0 +1,8 @@
+University_list = ["Ateneo de Manila University", "Aklan State University Main Campus", "Polytechnic University of the Philippines", "University of the Philippines Diliman", "Philippine Normal University - Manila", "Western Mindanao State University", "Ateneo de Naga University", "Mariano Marcos State University Main Campus", "Angeles University Foundation", "Bulacan State University"]
+print("A. Entire list:", University_list)
+print("B. 6th index is:", University_list[5])
+University_list[3] = "Harvard University"
+print("C. Updated List:", University_list)
+del University_list[8]
+print("D. Updated List:", University_list)
+print("F. Last Index Is:", University_list[-1])
