2b2d8fa63d317c57562ced0028107e54a1def4cc
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 16:10:12 +0800

    Famous Celebrity Actors

---
+++ actor_list.py
@@ -0,0 +1,8 @@
+Actor_list = ["Robert Downey Jr.", "Tom Hanks", "Morgan Freeman", "Brad Pitt", "Samuel L. Jackson", "Johnny Depp", "Will Smith", "Tom Cruise", "Denzel Washington", "Robin Williams"]
+print("A. Entire list:", Actor_list)
+print("B. 7th index is:", Actor_list[6])
+Actor_list[2] = "Leonardo DiCaprio"
+print("C. Updated List:", Actor_list)
+del Actor_list[7]
+print("D. Updated List:", Actor_list)
+print("F. Last Index Is:", Actor_list[-1])
