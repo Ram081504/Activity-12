fca775f14e454dad74dbc892aff54bc0cb6d3d8e
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 15:40:37 +0800

    Music Genres

---
+++ music_genre_list.py
@@ -0,0 +1,9 @@
+Music_genre_list = ["Pop Music", "Rock", "Rhythm and Blues", "Popular Music", "Electronic Music", "Country Music", "Blues", "Hip Hop Music", "Classical Music", "Heavy Metal", "Alternative Rock", "Funk", "World Music", "Indie Rock", "Folk Music"]
+print("A. Entire list:", Music_genre_list)
+print("B. 10th index is:", Music_genre_list[9])
+Music_genre_list[4] = "Jazz"
+print("C. Updated List:", Music_genre_list)
+del Music_genre_list[6]
+print("D. Updated List:", Music_genre_list)
+print("E. Sliced Portion:", Music_genre_list[2:8])
+print("F. Last Index Is:", Music_genre_list[-1])
