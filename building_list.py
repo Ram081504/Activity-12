cf0a0d677f5bc42baf4c335edc2f699140d26510
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 15:48:01 +0800

    Famous Tallest Building

---
+++ building_list.py
@@ -0,0 +1,8 @@
+Building_list = ["Taj Mahal", "Colosseum", "St. Basils Cathedral", "Empire State Building", "Tower of Pisa", "Basílica de la Sagrada Família", "Sydney Opera House", "Burj Khalifa"]
+print("A. Entire list:", Building_list)
+print("B. 5th index is:", Building_list[4])
+Building_list[1] = "Eiffel Tower"
+print("C. Updated List:", Building_list)
+del Building_list[5]
+print("D. Updated List:", Building_list)
+print("E. Last Index Is:", Building_list[-1])
