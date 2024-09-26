41237069ce6b48e482fddd7966b7210d27bbb9f3
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 17:47:02 +0800

    Tools that use in Carpentry

---
+++ tool_list.py
@@ -0,0 +1,8 @@
+Tools_list = ["Circular Saw", "Chisel", "Tape Measure", "Hand Saw", "Power Drill", "Screwdriver", "Pencil", "Squares", "Level", "Miter Saw"]
+print("A. Entire list:", Tools_list)
+print("B. 4th index is:", Tools_list[3])
+Tools_list[6] = "Hammer"
+print("C. Updated List:", Tools_list)
+del Tools_list[4]
+print("D. Updated List:", Tools_list)
+print("F. Last Index Is:", Tools_list[-1])
