0b3ff4bb290bb50ac8f33fe2165b28e15c4080b1
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 20:05:52 +0800

    World Currency

---
+++ currency_list.py
@@ -0,0 +1,8 @@
+Currency_list = ["US Dollar", "British Pound", "Canadian Dollar", "Australian Dollar", "Japanese Yen", "Cardano", "Emirati Dirham", "Afghan Afghani", "Albanian Lek", "Armenian Dram"]
+print("A. Entire list:", Currency_list)
+print("B. 4th index is:", Currency_list[3])
+Currency_list[6] = "Euro"
+print("C. Updated List:", Currency_list)
+del Currency_list[8]
+print("D. Updated List:", Currency_list)
+print("F. Last Index Is:", Currency_list[-1])
