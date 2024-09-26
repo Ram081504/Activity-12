87eb38640e1293089d6a8d7b97c4460c176a2629
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 14:31:11 +0800

    Delicious Desserts

---
+++ dessert_list.py
@@ -0,0 +1,8 @@
+Dessert_list = ["Cookies", "Milk Cakes", "Biscuits", "Pastries", "Confections", "Custards and Pudding", "Deep-fried", "Ice Cream", "Gelatin", "Fruits"]
+print("A. Entire list:", Dessert_list)
+print("B. 4th index is:", Dessert_list[3])
+Dessert_list[5] = "Chocolate Cake"
+print("C. Updated List:", Dessert_list)
+del Dessert_list[2]
+print("D. Updated List:", Dessert_list)
+print("F. Last Index Is:", Dessert_list[-1])
