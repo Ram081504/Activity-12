e295fbdf24f4f77977e06f5b1239be3c74916a3a
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 14:40:01 +0800

    Festivals Around the World

---
+++ festival_list.py
@@ -0,0 +1,9 @@
+Festival_list = ["Dia de los Muertos", "Songkran water festival", "Rio Carnival", "Mardi Gras in New Orleans", "Holi", "La Tomatina", "Oktoberfest", "Burning Man", "Venice Carnival", "Holy Week in Spain", "St. Patrick's Day", "Harbin International Ice and Snow Festival", "Glastonbury", "Albuquerque International Balloon Fiesta", "Montreal International Jazz Festival"]
+print("A. Entire list:", Festival_list)
+print("B. 7th index is:", Festival_list[6])
+Festival_list[10] = "Diwali"
+print("C. Updated List:", Festival_list)
+del Festival_list[8]
+print("D. Updated List:", Festival_list)
+print("E. Sliced Portion:", Festival_list[4:12])
+print("F. Last Index Is:", Festival_list[-1])
