6420819e5d59e904a1f0501ff0d3e04299a447cf
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 14:21:33 +0800

    Holidays in the Philippines

---
+++ holiday_list.py
@@ -0,0 +1,9 @@
+Holiday_list = ["Independence Day", "Labor Day", "Good Friday", "National Heroes' Day", "Maundy Thurday", "Rizal Day", "Bonifacio Day", "Ninoy Aquino Day", "New Year's Day", "Araw ng Kagitingan", "Black Saturday", "Chinese New Year"]
+print("A. Entire list:", Holiday_list)
+print("B. 9th index is:", Holiday_list[8])
+Holiday_list[2] = "Christmas"
+print("C. Updated List:", Holiday_list)
+del Holiday_list[10]
+print("D. Updated List:", Holiday_list)
+print("E. Sliced Portion:", Holiday_list[1:7])
+print("F. Last Index Is:", Holiday_list[-1])
