9faeb176ddeb1d350855587c531f509c076df12e
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 15:09:15 +0800

    Important Furniture in the House

---
+++ furniture_list.py
@@ -0,0 +1,8 @@
+Furniture_list = ["chair", "Table", "Beds", "Cabinet", "Coffee Table", "Desk", "Stool", "Tv Stand", "Wardrobe", "Nightstand", "Ottoman", "Recliner"]
+print("A. Entire list:", Furniture_list)
+print("B. 8th index is:", Furniture_list[7])
+Furniture_list[4] = "Sofa"
+print("C. Updated List:", Furniture_list)
+del Furniture_list[9]
+print("D. Updated List:", Furniture_list)
+print("F. Last Index Is:", Furniture_list[-1])
