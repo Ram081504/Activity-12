71b9e9486f20921727b9836374b239adb21eefd0
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 22:49:20 +0800

    Refreshing List of Drinks

---
+++ drink_list.py
@@ -0,0 +1,8 @@
+Drink_list = ["Buko Juice", "Melon Juice", "Pineapple Juice", "Guyabano Juice", "Guinumis", "Buko Pandan Drink", "Apple Juice", "Mango Juice"]
+print("A. Entire list:", Drink_list)
+print("B. 4th index is:", Drink_list[3])
+Drink_list[2] = "Coffee"
+print("C. Updated List:", Drink_list)
+del Drink_list[6]
+print("D. Updated List:", Drink_list)
+print("F. Last Index Is:", Drink_list[-1])
