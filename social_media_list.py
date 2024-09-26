2323d0e8823b0e0ef09a18f81182cc00ea6e9be7
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 15:24:20 +0800

    Different Social Media Platform

---
+++ social_media_list.py
@@ -0,0 +1,8 @@
+Social_Media_list = ["Facebook", "Snapchat", "Youtube", "TikTok", "WhatsApp", "Pinterest", "WeChat", "Telegram", "Linkedln", "Reddit"]
+print("A. Entire list:", Social_Media_list)
+print("B. 5th index is:", Social_Media_list[4])
+Social_Media_list[2] = "Instagram"
+print("C. Updated List:", Social_Media_list)
+del Social_Media_list[7]
+print("D. Updated List:", Social_Media_list)
+print("F. Last Index Is:", Social_Media_list[-1])
