238afb23a1c5493f9133dce07eaebc5da7795a09
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 14:49:13 +0800

    Continent of the World

---
+++ continent_list.py
@@ -0,0 +1,8 @@
+Continent_list = ["Asia", "Europe", "North America", "South America", "Antartica", "Autralia", "Zealandia"]
+print("A. Entire list:", Continent_list)
+print("B. 4th index is:", Continent_list[3])
+Continent_list[1] = "Africa"
+print("C. Updated List:", Continent_list)
+del Continent_list[5]
+print("D. Updated List:", Continent_list)
+print("E. Last Index Is:", Continent_list[-1])
