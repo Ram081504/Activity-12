3a205c93f84cce026db18194e19879d34b00822f
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 15:32:27 +0800

    Most Popular Sports

---
+++ sports_list.py
@@ -0,0 +1,8 @@
+Sports_list = ["Football", "Cricket", "Hockey", "Tennis", "Volleyball", "Baseball", "Rugby", "Golf", "Snooker", "Boxing"]
+print("A. Entire list:", Sports_list)
+print("B. 6th index is:", Sports_list[5])
+Sports_list[3] = "Basketball"
+print("C. Updated List:", Sports_list)
+del Sports_list[8]
+print("D. Updated List:", Sports_list)
+print("F. Last Index Is:", Sports_list[-1])
