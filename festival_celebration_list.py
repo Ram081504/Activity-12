24b50fd59bea75b0fcb5c9cf29333a2fff24cd7f
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 23:06:19 +0800

    Festival Celebrations

---
+++ festival_celebration_list.py
@@ -0,0 +1,8 @@
+Festival_Celebration_list = ["Giant Lantern Festival", "Sinulog festival", "Songkran", "Ati-Atihan festival", "Día de los Muertos", "Mardi Gras in New Orleans", "MassKara Festival", "Carnival", "Venice Carnival", "Catandungan Festival"]
+print("A. Entire list:", Festival_Celebration_list)
+print("B. 5th index is:", Festival_Celebration_list[4])
+Festival_Celebration_list[2] = "Thanksgiving"
+print("C. Updated List:", Festival_Celebration_list)
+del Festival_Celebration_list[6]
+print("D. Updated List:", Festival_Celebration_list)
+print("F. Last Index Is:", Festival_Celebration_list[-1])
