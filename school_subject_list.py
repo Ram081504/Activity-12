ef777fd056f0518463e30030163236d641ab777e
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 17:28:38 +0800

    Subjects in the Philippines Schools

---
+++ school_subject_list.py
@@ -0,0 +1,9 @@
+School_Subject_list = ["Science", "Physical Education", "Art", "Contemporary Arts", "Earth and Life Science", "English", "Media and information Literacy", "Oral Communication", "Art Appreciation", "Data Structures in Algorithms", "Platform Technologies", "Object-Oriented Programming"]
+print("A. Entire list:", School_Subject_list)
+print("B. 6th index is:", School_Subject_list[5])
+School_Subject_list[7] = "Mathematics"
+print("C. Updated List:", School_Subject_list)
+del School_Subject_list[3]
+print("D. Updated List:", School_Subject_list)
+print("E. Sliced Portion:", School_Subject_list[1:5])
+print("F. Last Index Is:", School_Subject_list[-1])
