bec27f7f1b04997f6a025092e4a51127ddbb0cb8
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 14:01:06 +0800

    8 Famous Invention in the World

---
+++ invention_list.py
@@ -0,0 +1,8 @@
+Invention_list = ["Steam Engine", "Smartphone", "Computer", "Printing Press", "Airplane", "Concrete", "Internal Combustion Engine", "Paper"]
+print("A. Entire list:", Invention_list)
+print("B. 6th index is:", Invention_list[5])
+Invention_list[1] = "Lightbulb"
+print("C. Updated List:", Invention_list)
+del Invention_list[4]
+print("D. Updated List:", Invention_list)
+print("F. Last Index Is:", Invention_list[-1])
