61af4ef13b961b885b366656c5b3ad61a69fe4a0
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 18:02:24 +0800

    Greatest Technologies Inventions

---
+++ technology_inventions_list.py
@@ -0,0 +1,9 @@
+Technology_Inventions_list = ["Computer", "Internet", "Lightbulb", "Airplane", "Artificial Intelligence", "Camera", "Microwave Ovens", "Printing Press", "3D Printer", "Edison cylinder phonograph", "Transistors", "Steam Engine", "Automobile", "The Internet", "The compass"]
+print("A. Entire list:", Technology_Inventions_list)
+print("B. 11th index is:", Technology_Inventions_list[10])
+Technology_Inventions_list[7] = "Smartphone"
+print("C. Updated List:", Technology_Inventions_list)
+del Technology_Inventions_list[13]
+print("D. Updated List:", Technology_Inventions_list)
+print("E. Sliced Portion:", Technology_Inventions_list[4:10])
+print("F. Last Index Is:", Technology_Inventions_list[-1])
