dae828dadd41160a999ab98ed3cad6c380fcfd10
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-26 18:39:36 +0800

    Oceans of the World

---
+++ ocean_list.py
@@ -0,0 +1,8 @@
+Ocean_list = ["Arctic", "Atlantic", "Antarctic", "Indian", "North Pacific"]
+print("A. Entire list:", Ocean_list)
+print("B. 3rd index is:", Ocean_list[2])
+Ocean_list[1] = "Pacific Ocean"
+print("C. Updated List:", Ocean_list)
+del Ocean_list[3]
+print("D. Updated List:", Ocean_list)
+print("E. Last Index Is:", Ocean_list[-1])
