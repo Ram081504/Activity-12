da3fc1d58fbb28459c3320c93ce455d120589626
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-27 16:21:47 +0800

    Biggest Volcano

---
+++ volcan_list.py
@@ -0,0 +1,16 @@
+Volcano_list = [
+    "Mount Fuji",
+    "Mount Kilimanjaro",
+    "Mount Etna",
+    "Mauna Loa",
+    "Mount Vesuvius",
+    "Mount St. Helens",
+    "Mount Pinatubo"
+]
+print("A. Entire list:", Volcano_list)
+print("B. 4th index is:", Volcano_list[3])
+Volcano_list[2] = "Mount Vesuvius"
+print("C. Updated List:", Volcano_list)
+del Volcano_list[4]
+print("D. Updated List:", Volcano_list)
+print("F. Last Index Is:", Volcano_list[-1])
