6129984213e3571eb34215bca2d3deacbb479b88
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-27 16:31:32 +0800

    Famous Bridges

---
+++ famous_bridge_list.py
@@ -0,0 +1,17 @@
+Famous_Bridge_list = [
+    "Tower Bridge",
+    "Brooklyn Bridge",
+    "Sydney Harbour Bridge",
+    "Golden Gate Bridge",
+    "Ponte Vecchio",
+    "Akashi Kaikyō Bridge",
+    "The Verrazano-Narrows Bridge",
+    "George Washington Bridge"
+]
+print("A. Entire list:", Famous_Bridge_list)
+print("B. 3rd index is:", Famous_Bridge_list[2])
+Famous_Bridge_list[5] = "Golden Gate Bridge"
+print("C. Updated List:", Famous_Bridge_list)
+del Famous_Bridge_list[6]
+print("D. Updated List:", Famous_Bridge_list)
+print("F. Last Index Is:", Famous_Bridge_list[-1])
