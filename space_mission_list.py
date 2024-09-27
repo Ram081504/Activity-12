dc3d753bb211cf7158ecf2c63dc0230bcdb4067a
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-27 16:40:34 +0800

    Space Missions

---
+++ space_mission_list.py
@@ -0,0 +1,19 @@
+Space_Missions_list = [
+    "Sputnik 1",
+    "Vostok 1",
+    "Mercury-Atlas 6",
+    "Gemini 4",
+    "Apollo 10",
+    "Soyuz 1",
+    "Apollo 11",
+    "Voyager 1",
+    "Hubble Space Telescope",
+    "International Space Station"
+]
+print("A. Entire list:", Space_Missions_list)
+print("B. 7th index is:", Space_Missions_list[6])
+Space_Missions_list[3] = "Apollo 11"
+print("C. Updated List:", Space_Missions_list)
+del Space_Missions_list[5]
+print("D. Updated List:", Space_Missions_list)
+print("F. Last Index Is:", Space_Missions_list[-1])
