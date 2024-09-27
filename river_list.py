393ee23afa9d1ba90dcdf047b949d45897baf601
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-27 16:16:39 +0800

    Famous Rivers

---
+++ river_list.py
@@ -0,0 +1,19 @@
+River_list = [
+    "Amazon River",
+    "Yangtze River",
+    "Mississippi River",
+    "Nile River",
+    "Congo River",
+    "Mekong River",
+    "Yellow River",
+    "Lena River",
+    "Amur River",
+    "Rio Grande"
+]
+print("A. Entire list:", River_list)
+print("B. 6th index is:", River_list[5])
+River_list[7] = "Nile River"
+print("C. Updated List:", River_list)
+del River_list[8]
+print("D. Updated List:", River_list)
+print("F. Last Index Is:", River_list[-1])
