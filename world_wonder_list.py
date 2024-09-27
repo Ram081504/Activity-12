99299226ef503892223dfb37f9eef25416c71fca
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-27 16:36:45 +0800

    Famous Wonder of the World

---
+++ world_wonder_list.py
@@ -0,0 +1,16 @@
+World_Wonders_list = [
+    "Great Pyramid of Giza",
+    "Chichen Itza",
+    "Colosseum",
+    "Christ the Redeemer",
+    "Machu Picchu",
+    "Petra",
+    "Taj Mahal"
+]
+print("A. Entire list:", World_Wonders_list)
+print("B. 4th index is:", World_Wonders_list[3])
+World_Wonders_list[1] = "Great Wall of China"
+print("C. Updated List:", World_Wonders_list)
+del World_Wonders_list[4]
+print("D. Updated List:", World_Wonders_list)
+print("F. Last Index Is:", World_Wonders_list[-1])
