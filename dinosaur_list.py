0edd97ed2179daf2b87fe41db5f93951af4f44af
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-27 15:58:10 +0800

    List of Dinosaur

---
+++ dinosaur_list.py
@@ -0,0 +1,8 @@
+Dinosaur_list = ["Aardonyx", "Abelisaurus", "Achillobator", "Aegyptosaurus", "Agilisaurus", "Acrocanthosaurus", "Afrovenator", "Amargasaurus"]
+print("A. Entire list:", Dinosaur_list)
+print("B. 4th index is:", Dinosaur_list[3])
+Dinosaur_list[5] = "Tyrannosaurus Rex"
+print("C. Updated List:", Dinosaur_list)
+del Dinosaur_list[6]
+print("D. Updated List:", Dinosaur_list)
+print("F. Last Index Is:", Dinosaur_list[-1])
