6b7f576ca12f6fb1bc9c22f0cc045a5adbf50018
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-27 16:26:32 +0800

    Famous Waterfalls

---
+++ waterfall_list.py
@@ -0,0 +1,19 @@
+Waterfalls_list = [
+    "Victoria Falls",
+    "Angel Falls",
+    "Iguazu Falls",
+    "Niagara Falls",
+    "Kaieteur Falls",
+    "Tugela Falls",
+    "Rhine Falls",
+    "Dettifoss",
+    "Yosemite Falls",
+    "Plitvice Lakes National Park"
+]
+print("A. Entire list:", Waterfalls_list)
+print("B. 8th index is:", Waterfalls_list[7])
+Waterfalls_list[4] = "Niagara Falls"
+print("C. Updated List:", Waterfalls_list)
+del Waterfalls_list[8]
+print("D. Updated List:", Waterfalls_list)
+print("F. Last Index Is:", Waterfalls_list[-1])
