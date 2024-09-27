a2fcad01306673506b07edbcaa8b99264b3bb444
Ram081504 <ramlouisecapilitan015@gmail.com>
2024-09-27 16:05:16 +0800

    Natural Landmark in the World

---
+++ natural_landmark_list.py
@@ -0,0 +1,8 @@
+Natural_Landmark_list = ["Victoria Falls", "Puerto Princesa Subterranean River National Park", "Uluru", "Hạ Long Bay", "Uyuni Salt Flat", "Cliffs of Moher", "Nā Pali Coast State Wilderness Park", "Verdon Gorge", "Pangong Tso", "Chocolate Hills"]
+print("A. Entire list:", Natural_Landmark_list)
+print("B. 8th index is:", Natural_Landmark_list[7])
+Natural_Landmark_list[4] = "Grand Canyon"
+print("C. Updated List:", Natural_Landmark_list)
+del Natural_Landmark_list[8]
+print("D. Updated List:", Natural_Landmark_list)
+print("F. Last Index Is:", Natural_Landmark_list[-1])
